18abc6a7d32432fbfa27c19545da7c72139b9f10
Justin Glenn Smith <noisesmith@gmail.com>
2016-05-23 11:52:42 -0700

    better info

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.noisesmith/git-info-edn "0.1.1"
+(defproject org.noisesmith/git-info-edn "0.2.0"
   :description "FIXME: write description"
   :url "http://github.com/noisesmith/lein-git-info-edn"
   :license {:name "Eclipse Public License"
--- src/leiningen/git_info_edn.clj
+++ src/leiningen/git_info_edn.clj
@@ -1,10 +1,16 @@
 (ns leiningen.git-info-edn
-  (:require [clojure.java.shell :as sh]))
+  (:require [clojure.java.shell :as sh]
+            [clojure.string :as string])
+  (:import (java.util Date)))
 
 (defn git-info-edn
   "adds git info to the resources directory"
   [& _]
   (spit "resources/deploy-info.edn"
-        (pr-str {:hash (:out (sh/sh "git" "rev-parse" "HEAD"))
-                 :status (:out (sh/sh "git" "status"))}))
+        (string/join \newline
+                     (map (partial apply print-str)
+                          {:hash (:out (sh/sh "git" "rev-parse" "HEAD"))
+                           :status (:out (sh/sh "git" "status"))
+                           :date (Date.)
+                           \space \space})))
   true)
