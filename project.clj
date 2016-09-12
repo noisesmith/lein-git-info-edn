b9acee5cc8fed4266c151fa4fef4d228ac847af6
Justin Glenn Smith <noisesmith@gmail.com>
2016-09-12 11:54:22 -0700

    version bump for new deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.noisesmith/git-info-edn "0.2.0"
+(defproject org.noisesmith/git-info-edn "0.2.1"
   :description "FIXME: write description"
   :url "http://github.com/noisesmith/lein-git-info-edn"
   :license {:name "Eclipse Public License"
