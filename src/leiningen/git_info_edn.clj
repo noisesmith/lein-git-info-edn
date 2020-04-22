f3d27a619900a9ebc96b5e43b65501b5e4a08194
Justin Glenn Smith <justin.smith@fundingcircle.com>
2020-04-22 09:24:26 -0700

    we want to make #inst even if the writer isn't installed

--- src/leiningen/git_info_edn.clj
+++ src/leiningen/git_info_edn.clj
@@ -1,13 +1,26 @@
 (ns leiningen.git-info-edn
-  (:require [clojure.java.shell :as sh])
-  (:import (java.util Date)))
+  (:require [clojure.java.io :as io]
+            [clojure.java.shell :as sh]
+            [clojure.string :as string]
+            [clojure.pprint :as pprint])
+  (:import (java.time Instant)
+           (java.io StringWriter)))
+
+(defn generate-info-str
+  []
+  (let [format-builder (StringWriter.)]
+    (pprint/pprint {:hash (string/trim (:out (sh/sh "git" "rev-parse" "HEAD")))
+                    :status (string/trim (:out (sh/sh "git" "status")))
+                    ;; inserts a format placeholder into the output
+                    :date  '%s}
+                    format-builder)
+    ;; this format approach is needed in order to support older clojure that doesn't generate #inst
+    (format (str format-builder)
+            (str "#inst \"" (Instant/now) "\""))))
 
 (defn git-info-edn
   "adds git info to the resources directory"
   [& _]
   (spit "resources/deploy-info.edn"
-    (pr-str
-      {:hash (:out (sh/sh "git" "rev-parse" "HEAD"))
-       :status (:out (sh/sh "git" "status"))
-       :date (Date.)}))
+        (generate-info-str))
   true)
