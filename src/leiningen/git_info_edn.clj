9677e99e9f6c2d3c9773f76f557f563d23394673
Justin Glenn Smith <justin.smith@fundingcircle.com>
2020-04-22 09:39:46 -0700

    another method, less hacky

--- src/leiningen/git_info_edn.clj
+++ src/leiningen/git_info_edn.clj
@@ -1,22 +1,24 @@
 (ns leiningen.git-info-edn
-  (:require [clojure.java.io :as io]
-            [clojure.java.shell :as sh]
+  (:require [clojure.java.shell :as sh]
             [clojure.string :as string]
             [clojure.pprint :as pprint])
-  (:import (java.time Instant)
-           (java.io StringWriter)))
+  (:import (java.time Instant)))
+
+(def date-emitter
+  "needed in order to support #inst output in older clojure versions"
+  (reify Object
+    (toString [this]
+      (str "#inst \"" (Instant/now) "\""))))
+
+(defmethod print-method (class date-emitter) [x p]
+  (.write p (str x)))
 
 (defn generate-info-str
   []
-  (let [format-builder (StringWriter.)]
-    (pprint/pprint {:hash (string/trim (:out (sh/sh "git" "rev-parse" "HEAD")))
-                    :status (string/trim (:out (sh/sh "git" "status")))
-                    ;; inserts a format placeholder into the output
-                    :date  '%s}
-                    format-builder)
-    ;; this format approach is needed in order to support older clojure that doesn't generate #inst
-    (format (str format-builder)
-            (str "#inst \"" (Instant/now) "\""))))
+  (with-out-str
+   (pprint/pprint {:hash (string/trim (:out (sh/sh "git" "rev-parse" "HEAD")))
+                   :status (string/trim (:out (sh/sh "git" "status")))
+                   :date date-emitter})))
 
 (defn git-info-edn
   "adds git info to the resources directory"
